cff63c92703768fb9590aae03e4850ebfd4276c5
Eri Haraguchi <eruru1023@icloud.com>
2023-05-17 12:30:35 +0900

    Add:アプリを日本語化し、タイムゾーンの設定を追加

--- Gemfile
+++ Gemfile
@@ -49,6 +49,10 @@ gem 'sassc-rails'
 gem 'cssbundling-rails'
 gem 'jsbundling-rails'
 
+# 日本語化
+gem 'rails-i18n', '~> 7.0.0'
+gem 'enum_help'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,15 @@ module Okogoto
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    # タイムゾーンの設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone= :local
+
+    # 日本語化の設定
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+
     config.generators do |g|
       g.skip_routes true
       g.assets false
