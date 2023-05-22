a8e34387b4a885ab12c7f493fd1b798e9121fac2
Eri Haraguchi <eruru1023@icloud.com>
2023-05-23 08:40:44 +0900

    Add:フラッシュメッセージを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      redirect_to login_path, flash: { success: t('.success') }
     else
-      render :new
+      flash.now[:error] = t('.fail')
+      render :new, status: :unprocessable_entity
     end
   end
 
