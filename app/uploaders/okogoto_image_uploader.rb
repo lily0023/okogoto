cee12881416e2d7e7cdcfc5ab631ce1ee1ed9ea3
Eri Haraguchi <eruru1023@icloud.com>
2023-05-27 03:33:32 +0900

    Add:本番環境で画像を保存するためのAWSの設定を追加

--- app/uploaders/okogoto_image_uploader.rb
+++ app/uploaders/okogoto_image_uploader.rb
@@ -4,8 +4,13 @@ class OkogotoImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -35,9 +40,9 @@ class OkogotoImageUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
