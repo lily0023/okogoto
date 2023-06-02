1fe6463a71871e93bc7f6fa7c210e995a2d2422e
Eri Haraguchi <eruru1023@icloud.com>
2023-06-03 03:09:52 +0900

    Add:メールアドレスにフォームを送信する形のパスワードリセット機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_074101) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_075313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_074101) do
     t.integer "gender", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "likes", "posts"
