668a3b8a08d57be64ccf0fa21503b580e5dd0774
Eri Haraguchi <eruru1023@icloud.com>
2023-06-06 23:29:34 +0900

    Fix:オコゴトが未入力の時オコゴト画像が生成されないようにする

--- app/controllers/okogoto_images_controller.rb
+++ app/controllers/okogoto_images_controller.rb
@@ -5,20 +5,22 @@ class OkogotoImagesController < ApplicationController
 
   def create
     okogoto = kogoto_params[:okogoto]
-    okogoto_image = case params[:image]
-                    when 'kakugen'
-                      KakugenImage.build(okogoto)
-                    when 'kogoto'
-                      KogotoImage.build(okogoto)
-                    when 'friends'
-                      FriendsImage.build(okogoto)
-                    end
-    @post = current_user.posts.new(kogoto_image: okogoto_image)
-    if @post.save
-      redirect_to edit_post_path(@post), flash: { success: t('.success') }
-    else
-      flash.now[:error] = t('.fail')
+
+    if okogoto.blank?
+      flash.now[:error] = 'オコゴトを入力してください'
       render :new, status: :unprocessable_entity
+    else
+      okogoto_image = case params[:image]
+                      when 'kakugen'
+                        KakugenImage.build(okogoto)
+                      when 'kogoto'
+                        KogotoImage.build(okogoto)
+                      when 'friends'
+                        FriendsImage.build(okogoto)
+                      end
+      @post = current_user.posts.new(kogoto_image: okogoto_image)
+      @post.save
+      redirect_to edit_post_path(@post), flash: { success: t('.success') }
     end
   end
 
