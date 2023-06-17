b6d08b727a9ae40f20131949021979401381bd61
Eri Haraguchi <eruru1023@icloud.com>
2023-06-18 07:08:09 +0900

    Add:ユーザー情報に役職を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,14 +4,15 @@
 #
 #  id                                  :bigint           not null, primary key
 #  access_count_to_reset_password_page :integer          default(0)
-#  age                                 :integer          default(0), not null
+#  age                                 :integer          default("secret"), not null
 #  crypted_password                    :string
 #  email                               :string           not null
-#  gender                              :integer          default(0), not null
+#  gender                              :integer          default("others"), not null
 #  name                                :string           not null
 #  reset_password_email_sent_at        :datetime
 #  reset_password_token                :string
 #  reset_password_token_expires_at     :datetime
+#  role                                :integer          default(0), not null
 #  salt                                :string
 #  created_at                          :datetime         not null
 #  updated_at                          :datetime         not null
